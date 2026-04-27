a7293509f074d60feeb36902e68374203c4c2481
kaclaes <kaclaes@student.42belgium.be>
2026-04-27 15:35:45 +0200

    add ft_lstclear.c ft_lstiter.c

---
+++ ft_lstclear.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaclaes <kaclaes@student.42belgium.be>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/27 15:16:23 by kaclaes           #+#    #+#             */
+/*   Updated: 2026/04/27 15:29:49 by kaclaes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list	*curr;
+	t_list	*next;
+
+	if (!lst || !del)
+		return ;
+	curr = *lst;
+	while (curr)
+	{
+		next = curr->next;
+		(*del)(curr->content);
+		free(curr);
+		curr = next;
+	}
+	*lst = NULL;
+}
