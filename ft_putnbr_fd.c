c974853cb99cdde708c2259be3e9bddbc088b563
kaclaes <kaclaes@student.42belgium.be>
2026-04-30 17:51:58 +0200

    fix ft_putnbr_fd.c

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: kaclaes <kaclaes@student.42belgium.be>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/25 21:05:57 by kaclaes           #+#    #+#             */
-/*   Updated: 2026/04/28 17:58:19 by kaclaes          ###   ########.fr       */
+/*   Updated: 2026/04/30 17:51:34 by kaclaes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,28 @@
 #include <stddef.h>
 #include <unistd.h>
 
+static size_t	fill_str(char *tmp_str, long nbr);
+
 void	ft_putnbr_fd(int n, int fd)
 {
 	char	tmp_str[11];
-	size_t	i;
-	long	nbr;
-	int		negative;
-
-	nbr = n;
-	negative = 0;
+	size_t	offset;
+	
 	if (n == 0)
 	{
 		write(fd, "0", 1);
 		return ;
 	}
+	offset = fill_str(tmp_str, n);
+	write(fd, tmp_str + offset + 1, 10 - offset);
+}
+
+static size_t	fill_str(char *tmp_str, long nbr)
+{
+	int		negative;
+	size_t	i;
+
+	negative = 0;
 	i = 10;
 	if (nbr < 0)
 	{
@@ -41,7 +49,7 @@ void	ft_putnbr_fd(int n, int fd)
 	}
 	if (negative)
 		tmp_str[i--] = '-';
-	write(fd, tmp_str + i + 1, 10 - i);
+	return (i);
 }
 
 // #include <stdlib.h>
